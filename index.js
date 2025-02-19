01600f81f964b2358fabd83ab5d5ee9f54f41a37
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-02-19 13:22:57 +0500

    helmet mongosanitize cors and bodyparser added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 const setup = require("./api/routes");
 
@@ -10,6 +14,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// set security HTTP headers
+app.use(helmet());
+
+// sanitize request data
+app.use(mongoSanitize());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+// parse json request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Custom logger format
 logger.token("status-format", (req, res) => {
@@ -27,7 +45,7 @@ app.use(logger(":method :url :status-format :status :response-time ms"));
 
 //test route
 app.get("/", (req, res) => {
-  res.send("Hello, Server is Running!");
+  res.send("Hello, Server is Up and Running!");
 });
 
 // mongo connection
